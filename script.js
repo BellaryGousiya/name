2f2f2b1a677548fe978154a6e98c1a23cbca93da
Gousiya <gousi568@gmail.com>
2026-02-03 11:28:25 +0530

    adding buttons

---
+++ script.js
@@ -0,0 +1,14 @@
+const btn=document.getElementById("btn");
+const output=document.getElementById("output");
+const output1 =document.getElementById("output1");
+const btn1 =document.getElementById("btn1");
+
+btn.addEventListener("click",function(){
+
+    output1.textContent="BELLARY GOUSIYA";
+    output.textContent="LOVE U KANNA";
+    btn1.textContent = "POTTI__KANNA";
+});
+
+
+ 
